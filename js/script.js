0521a42de637b153c1e3bd9ee9ba43fb6b850c91
TSB <122724396+tsbproject@users.noreply.github.com>
2023-09-21 02:39:59 +0100

    [committed]

---
+++ js/script.js
@@ -0,0 +1,54 @@
+var previousScroll = 0;
+
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                var currentScroll = $(this).scrollTop();
+
+                if (currentScroll > previousScroll) {
+                    // Scrolling down
+                    $('.navbar').css('height', '60px');
+                    $('.navbar').css('padding', '10px 0');
+                    $('.nav-link').css('padding', '5px 0');
+                    $('.logo').css('padding', '10px 0');
+                } else {
+                    // Scrolling up
+                    $('.navbar').css('height', '80px');
+                    $('.navbar').css('padding', '20px 0');
+                    $('.nav-link').css('padding', '100px 0');
+                    $('.logo').css('padding', '100px 0');
+                }
+
+                previousScroll = currentScroll;
+            });
+        });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.navbar').css('height', '60px');
+            $('.navbar').css('padding', '10px 0');
+            $('.nav-link').css('padding', '5px 0');
+            $('.logo').css('padding', '10px 0');
+        } else {
+            $('.navbar').css('height', '80px');
+            $('.navbar').css('padding', '20px 0');
+            $('.nav-link').css('padding', '100px 0');
+            $('.logo').css('padding', '100px 0');
+
+            
+        }  
+    }); 
+});
